baf4532747b6b374ff824ff98f2aa972cbe86e06
Maciej <kawka.maciej.93@gmail.com>
2018-06-12 01:05:53 +0200

    added comments explaining main class

--- NameGenerator.java
+++ NameGenerator.java
@@ -5,9 +5,14 @@ import java.lang.Integer;
 
 public class NameGenerator{
 	public static void main(String[] args){
+		//generate name
 		System.out.println(generateName());
+
+		//generate password but first check if args are passed
+		// possible further addition of checking if int bigger than n passed
 		if(args.length > 0  && args[0] != ""){
-		System.out.println(generatePassword(Integer.parseInt(args[0])));
+		int charLength = Integer.parseInt(args[0]);
+		System.out.println(generatePassword(charLength));
 		}else{
 		System.out.println(generatePassword(8));
 		}
